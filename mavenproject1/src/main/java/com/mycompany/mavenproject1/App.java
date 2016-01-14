09985a2169047ecedc7592ed72a3cf67d451d69a
MiguelJM <mikejm92@gmail.com>
2016-01-14 20:05:22 +0100

    Eso

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/App.java
@@ -0,0 +1,18 @@
+package com.mycompany.mavenproject1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( printHello() );
+    }
+
+    public static String printHello(  )
+    {
+        return "Hello World!";
+    }
+}
